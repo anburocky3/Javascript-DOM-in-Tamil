5f95a234d75447fb3cdd3f4a8f5c05186f6c35b0
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-01-11 03:32:47 +0530

    04 - What is JS BOM?

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,25 @@
 // window.prompt(); // Prompts for value
 // window.print(); // Prints current window
 // console.log(); // Helps to print data to console.
+
+// BOM - Browser Object Model
+// # Screen Obj
+console.log(window.screen); // returns the visitor's screen info
+
+// # Location Obj
+location.href = "https://google.com";
+// OR
+window.location.assign(url);
+window.location = url;
+window.location.href = url;
+
+// # History Obj
+history.back(); //- same as clicking back in the browser
+history.forward(); //- same as clicking forward in the browser
+
+// # Navigator Obj
+navigator.appVersion;
+navigator.platform;
+navigator.webdriver;
+navigator.product; //  returns the product name of the browser engine
+navigator.cookieEnabled;
