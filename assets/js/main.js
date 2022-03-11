a763ebb0c1b7869533092e196286bfa7f55112eb
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-03-11 16:34:46 +0530

    Initial

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// Window Object
+window.innerHeight, window.innerWidth;
+window.open(); // - open a new window
+window.close(); // - close the current window
+window.moveTo(500, 100); // - move the current window
+window.resizeTo(); // - resize the current window
+
+window.alert("Hello"); // Alerts
+window.prompt(); // Prompts for value
+window.print(); // Prints current window
+console.log(); // Helps to print data to console.
+
+console.log(this, window);
