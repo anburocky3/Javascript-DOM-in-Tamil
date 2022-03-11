2c7408ca38c9b2dd2ad27f2bb444a624e5797b6c
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-03-11 19:39:35 +0530

    Initial

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,3 @@
-// Window Object
-// window.innerHeight, window.innerWidth
-// window.open(); // - open a new window
-// window.close() // - close the current window
-// window.moveTo(500, 100); // - move the current window
-// window.resizeTo() // - resize the current window
-
-// window.alert("Hello"); // Alerts
-// window.prompt(); // Prompts for value
-// window.print(); // Prints current window
-// console.log(); // Helps to print data to console.
-
 // BOM - Browser Object Model
 // # Screen Obj
 console.log(window.screen); // returns the visitor's screen info
