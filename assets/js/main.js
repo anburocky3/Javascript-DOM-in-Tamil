09f7f8c477e7c2da019041b2140945b2f93115f1
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-03-19 15:08:46 +0530

    initial

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,23 @@
-// Change h1 text using Javascript
-document.body.children[0].children[0].innerText =
-  "Learn HTML DOM in Javascript - Tamil";
+// Object Example
+let person = {
+  name: "Anbuselvan", // properties
+  place: "Chennai", // properties
+  greetUser() {
+    // method
+    return `Hello my name is ${this.name} and i'm coming from ${this.place}.`;
+  },
+};
 
-// Find the text in the document programmatically.
-console.log(window.find("HTML DOM"));
+console.log(person.greetUser());
+
+// Challenge: Dynamic simple program to get name & place automatically from the user.
+const nameInput = prompt("What is your name?");
+const placeInput = prompt("Where are you coming from?");
+
+person.name = nameInput;
+person.place = placeInput;
+
+document.writeln("Hello " + person.name);
+
+// Additionally you can also do this.
+// document.body.innerHTML = `<h1>${person.greetUser()}</h1>`;
