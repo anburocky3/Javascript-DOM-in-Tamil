9ce1adb3db8eb1a257af434bc8ff3c8a7c1d9844
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-03-19 18:30:30 +0530

    preparing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,3 @@
-// Object Example
-let person = {
-  name: "Anbuselvan", // properties
-  place: "Chennai", // properties
-  greetUser() {
-    // method
-    return `Hello my name is ${this.name} and i'm coming from ${this.place}.`;
-  },
-};
-
-console.log(person.greetUser());
-
-// Challenge: Dynamic simple program to get name & place automatically from the user.
-const nameInput = prompt("What is your name?");
-const placeInput = prompt("Where are you coming from?");
-
-person.name = nameInput;
-person.place = placeInput;
-
-document.writeln("Hello " + person.name);
-
-// Additionally you can also do this.
-// document.body.innerHTML = `<h1>${person.greetUser()}</h1>`;
+// document.addEventListener("DOMContentLoaded", () => {
+//   alert("Loaded");
+// });
