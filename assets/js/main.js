45e2ddd535f813ecbcf46f89378d8c3823016754
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-03-20 22:17:40 +0530

    13-recorded

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,14 @@
 let inputFood = document.getElementById("input-food");
 let inputBtn = document.getElementById("input-btn");
-let response = document.getElementById("response");
-
-// console.log(inputFood, inputBtn);
+let foodContainer = document.getElementById("food-container");
 
 inputBtn.addEventListener("click", () => {
-  response.innerText = inputFood.value;
+  foodContainer.innerHTML += `<li class="food-item">${inputFood.value.toUpperCase()}</li>`;
 });
+
+// getElementsByClassName
+const foodItems = foodContainer.getElementsByClassName("food-item");
+
+let allFoods = [].map.call(foodItems, (food) => food.textContent);
+
+console.log(allFoods);
