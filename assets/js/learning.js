e374496b6b2a2d4c064f30129e096bd1a68f7a56
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-03-28 17:59:46 +0530

    19-planned

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -1,31 +1,41 @@
-// 1. Creating elements using createElement()
-function createAlertviaDOM(message) {
-  const main = document.getElementById("main");
-  const div = document.createElement("div");
-  const textNode = document.createTextNode(message);
-  // injecting textNode into div
-  div.append(textNode);
-  div.className = "alert";
-  main.prepend(div);
+const foodContainerEl = document.getElementById("food-container");
+
+// normal way
+console.time("normal way");
+for (let i = 1; i <= 1000; i++) {
+  const newPEl = document.createElement("p");
+  newPEl.textContent = `Food Item ${i}`;
+  newPEl.className = "food-item";
+  foodContainerEl.append(newPEl);
 }
+console.timeEnd("normal way");
 
-createAlertviaDOM("Hey, i'm the new div");
-
-// 1. creating element using template & innerHTML
-function createAlertViaTemplate(message) {
-  const div = `
-    <div class="alert">
-      ${message}
-    </div>
-  `;
-
-  return div;
+// documentFragment() way
+console.time("documentFragment way");
+let docFragment = document.createDocumentFragment();
+for (let i = 1; i <= 1000; i++) {
+  const newPEl = document.createElement("p");
+  newPEl.textContent = `Food Item ${i}`;
+  newPEl.className = "food-item";
+  docFragment.append(newPEl);
 }
+foodContainerEl.append(docFragment);
+console.timeEnd("documentFragment way");
 
-const main = document.getElementById("main");
+// Challenge: Create your favourite foods using createElement & documentFragment
+const favouriteFood = [
+  "Chicken Biriyani",
+  "Mutton Biriyani",
+  "Fish Biriyani",
+  "Prawn Biriyani",
+];
 
-const onFoodItemsAdded = createAlertViaTemplate(
-  "Food items have been added Successfully!"
-);
+const listFragment = document.createDocumentFragment();
+favouriteFood.forEach((favFood) => {
+  let li = document.createElement("li");
+  li.textContent = favFood;
+  li.className = "food-item";
+  listFragment.append(li);
+});
 
-main.innerHTML += onFoodItemsAdded;
+foodContainerEl.append(listFragment);
