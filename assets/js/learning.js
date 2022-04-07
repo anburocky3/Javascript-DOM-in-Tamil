5c5db3868f1ca4ef97a9bc3f98d497620f2f98f5
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-04-07 22:55:23 +0530

    17-recorded

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -1,16 +1,40 @@
-function createAlertViaDOM(message) {
-  const main = document.getElementById("main");
+// function createAlertViaDOM(message) {
+//   const main = document.getElementById("main");
 
-  // Creating div element
-  const div = document.createElement("div");
-  const textNode = document.createTextNode(message);
+//   // Creating div element
+//   const div = document.createElement("div");
+//   const textNode = document.createTextNode(message);
 
-  // Assigning className
-  div.className = "alert";
+//   // Assigning className
+//   div.className = "alert";
 
-  // Append
-  div.append(textNode);
-  main.prepend(div);
-}
+//   // Append
+//   div.append(textNode);
+//   main.prepend(div);
+// }
 
-createAlertViaDOM("Food is updated!");
+// createAlertViaDOM("Food is updated!");
+
+// ---------------------------
+// Modify alert content
+/* 
+These are setter/getter
+- innerHTML
+- innerText // Works with Elements
+- textContent // Works with Nodes 
+*/
+
+const alertEl = document.querySelector(".alert .alert-message"); // https://jsben.ch/8tEs3 // Check benchmark here
+
+// how to get existing contents
+console.log(alertEl.innerText);
+console.log(alertEl.innerHTML);
+console.log(alertEl.textContent);
+
+// setting
+alertEl.innerText =
+  "<strong>✅ Successful!</strong> Hello, i've been modified!";
+alertEl.innerHTML =
+  "<strong>✅ Successful!</strong> Hello, i've been modified!";
+alertEl.textContent =
+  "<strong>✅ Successful!</strong> Hello, i've been modified!";
