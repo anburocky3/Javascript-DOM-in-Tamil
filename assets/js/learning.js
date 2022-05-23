aedebee8d64be6cf2d24e7ed02584ff9111760c0
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-05-23 22:00:56 +0530

    28-recorded

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -1,9 +1,11 @@
 const numbers = document.querySelector("ul:last-child li");
 
-// Any DOM Nodes
+console.log(numbers.innerText);
+
+//  Any Node : Node
 console.log(numbers.nextSibling);
 console.log(numbers.previousSibling);
 
-// Any Element Nodes
+// Any Element Nodes : Element
 console.log(numbers.nextElementSibling.innerText);
 console.log(numbers.previousElementSibling.innerText);
