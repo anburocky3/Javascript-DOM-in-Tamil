50f6f49132530803ef8ac5b6cc13e048f541c870
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-05-25 22:08:09 +0530

    29-planned

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -1,11 +1,21 @@
-const numbers = document.querySelector("ul:last-child li");
+const input = document.querySelector("#nameInput");
 
-console.log(numbers.innerText);
+// You can get attributes of html element using object syntax. (DOT Notation)
+console.log(input.type);
+console.log(input.id);
+console.log(input.name);
+console.log(input.placeholder);
 
-//  Any Node : Node
-console.log(numbers.nextSibling);
-console.log(numbers.previousSibling);
+// Modify the input value.
+input.value = "Trisha";
+input.value = 3;
+input.setAttribute("value", 3);
+console.log(typeof input.value);
+console.log(typeof parseInt(input.value, 10));
 
-// Any Element Nodes : Element
-console.log(numbers.nextElementSibling.innerText);
-console.log(numbers.previousElementSibling.innerText);
+// However, the web browser only converts the standard attributes to the DOM object’s properties.
+console.log(input.isAlive);
+
+// Task: Create an input field of Age, which accepts number and change it via DOM using Javascript.
+// input.value = 26;
+// console.log(`My age is: ${26}`);
