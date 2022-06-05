feaab75e0dd7309043066bfeef83897140f81a9e
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-06-05 12:58:46 +0530

    34-planned

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -1,17 +1,37 @@
-// Task: Show div based on click (toggle)
-const inputContainer = document.querySelector(".input-name-container");
-const showBtnEl = document.querySelector("#showBtn");
+const btnEl = document.querySelector("#creditBtn");
 
-showBtnEl.addEventListener("click", () => {
-  if (inputContainer.classList.contains("hide")) {
-    showBtnEl.innerText = "Hide Div";
-    showBtnEl.style.backgroundColor = "gray";
-    // inputContainer.style.display = "block";
-    inputContainer.classList.remove("hide");
-  } else {
-    // inputContainer.style.display = "none";
-    inputContainer.classList.add("hide");
-    showBtnEl.style.backgroundColor = "royalblue";
-    showBtnEl.innerText = "Show Div";
-  }
+console.log(btnEl);
+
+// a click on <body> will generate errors,
+// because attributes are always strings, function becomes a string
+document.body.setAttribute("onclick", function () {
+  alert(1);
 });
+
+// Drawback: Don't allows multiple events
+btnEl.onclick = function (event) {
+  // alert("I have been clicked");
+  alert(event.type + " at " + event.currentTarget.innerHTML);
+  alert("Coordinates: " + event.clientX + ":" + event.clientY);
+};
+
+// window.addEventListener("contextmenu", (e) => {
+//   // e.preventDefault();
+//   console.log(e);
+// });
+
+// const handleCreditEvent = function () {
+//   alert("Javascript DOM by Anbuselvan.");
+//   console.log(this);
+// };
+
+const handleCreditEvent = (event) => {
+  console.log(this); // window object
+  console.log(event.target); // event that is happening
+};
+
+btnEl.addEventListener("click", function (event) {
+  alert("Javascript DOM by Anbuselvan.");
+});
+
+btnEl.addEventListener("contextmenu", handleCreditEvent); //
