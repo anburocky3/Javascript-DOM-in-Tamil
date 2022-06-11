de007555d6b5e4478ece6b3f754baa63cd136c57
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-06-11 23:36:42 +0530

    37-planned

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -1,49 +1,8 @@
-const btnEl = document.querySelector("#creditBtn");
-const formEl = document.querySelector("form");
-const divEl = document.querySelector("form div");
-const pEl = document.querySelector("form p");
+const formEl = document.querySelector("form#learning");
+const inputTextEl = formEl.querySelector("input");
+const inputCheckboxEl = formEl.querySelector("input[type='checkbox']");
 
-pEl.addEventListener("click", () => {
-  alert("p");
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault(); // Stops default behaviour
+  console.log(inputTextEl.value, inputCheckboxEl.checked);
 });
-
-divEl.addEventListener("click", (event) => {
-  alert("div");
-
-  // Stopping Bubbling
-  event.stopPropagation();
-
-  // Removes all event listeners of that element
-  event.stopImmediatePropagation();
-});
-
-formEl.addEventListener("click", () => {
-  alert("Form");
-});
-
-formEl.addEventListener("click", (event) => {
-  event.target.style.color = "#FFF";
-
-  // chrome needs some time to paint yellow
-  setTimeout(() => {
-    alert("target = " + event.target.tagName + ", this= " + formEl.tagName);
-    event.target.style.color = "";
-  }, 0);
-});
-
-divEl.addEventListener("click", (event) => {
-  console.log("Another div click action", event);
-});
-
-// To catch an event on the capturing phase
-formEl.addEventListener("click", () => {}, { capture: true }); // or just "true", by default it is false
-
-// Catch both capturing and bubblnig
-for (let elem of document.querySelectorAll("form, form *")) {
-  elem.addEventListener(
-    "click",
-    (e) => alert(`Capturing: ${elem.tagName}`),
-    true
-  );
-  elem.addEventListener("click", (e) => alert(`Bubbling: ${elem.tagName}`));
-}
