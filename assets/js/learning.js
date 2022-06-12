f6861a73c7720eb5c67d292055ba116b69988d6b
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-06-12 23:26:04 +0530

    38-planned

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -1,8 +1,44 @@
-const formEl = document.querySelector("form#learning");
-const inputTextEl = formEl.querySelector("input");
-const inputCheckboxEl = formEl.querySelector("input[type='checkbox']");
+const tableEl = document.querySelector("table#authors");
+
+let selectedId;
+
+function handleTableClick(event) {
+  const target = event.target;
+  const closestTarget = target.closest("tr");
+
+  if (target.tagName == "TH") return; // Ignoring TH tag
+
+  if (selectedId != undefined) {
+    selectedId.classList.remove("active");
+  }
+
+  selectedId = closestTarget;
+
+  closestTarget.classList.add("active"); // applying .active class
+
+  console.log(`Hello ${target.textContent}`);
+  // console.log(target.closest("tr").children[0].textContent); // Better approach
+}
+
+tableEl.addEventListener("click", handleTableClick);
+
+// Task: Show a Donate Form on Toggle
+document.addEventListener("click", function (event) {
+  let id = event.target.dataset.toggleId;
+
+  if (!id) return;
+
+  let el = document.getElementById(id);
+
+  el.hidden = !el.hidden;
+});
+
+// handling the form
+const formEl = document.querySelector("form");
 
 formEl.addEventListener("submit", (event) => {
-  event.preventDefault(); // Stops default behaviour
-  console.log(inputTextEl.value, inputCheckboxEl.checked);
+  event.preventDefault();
+
+  const donatedAmount = event.target.querySelector("input").value;
+  alert(`Thank you for donating ₹${donatedAmount}.`);
 });
