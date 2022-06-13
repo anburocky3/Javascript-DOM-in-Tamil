af386a341423966bb4f7da821d2249814b5d441e
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-06-13 21:36:07 +0530

    36-recorded

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -1,49 +1,42 @@
-const btnEl = document.querySelector("#creditBtn");
 const formEl = document.querySelector("form");
 const divEl = document.querySelector("form div");
 const pEl = document.querySelector("form p");
 
-pEl.addEventListener("click", () => {
-  alert("p");
+formEl.addEventListener("click", (event) => {
+  alert("Form");
+  console.log(`Target: ${event.target.tagName}, this: ${formEl.tagName}`);
 });
 
 divEl.addEventListener("click", (event) => {
-  alert("div");
+  alert("Div");
 
-  // Stopping Bubbling
+  // // Stop Bubbling
   event.stopPropagation();
 
-  // Removes all event listeners of that element
+  // Removes all active event listeners
   event.stopImmediatePropagation();
 });
 
-formEl.addEventListener("click", () => {
-  alert("Form");
-});
-
-formEl.addEventListener("click", (event) => {
-  event.target.style.color = "#FFF";
-
-  // chrome needs some time to paint yellow
-  setTimeout(() => {
-    alert("target = " + event.target.tagName + ", this= " + formEl.tagName);
-    event.target.style.color = "";
-  }, 0);
-});
-
-divEl.addEventListener("click", (event) => {
-  console.log("Another div click action", event);
+pEl.addEventListener("click", () => {
+  alert("P");
 });
 
 // To catch an event on the capturing phase
-formEl.addEventListener("click", () => {}, { capture: true }); // or just "true", by default it is false
+formEl.addEventListener("click", () => {}, { capture: true }); // by default false,
 
-// Catch both capturing and bubblnig
+// Task: Catch the Capturing & Bubbling Phrase
 for (let elem of document.querySelectorAll("form, form *")) {
+  // Capturing Phase
   elem.addEventListener(
     "click",
-    (e) => alert(`Capturing: ${elem.tagName}`),
-    true
+    () => {
+      console.log(`Capturing Phase: ${elem.tagName}`);
+    },
+    { capture: true }
   );
-  elem.addEventListener("click", (e) => alert(`Bubbling: ${elem.tagName}`));
+
+  // Bubbling Phase
+  elem.addEventListener("click", () => {
+    console.log(`Bubbling Phase: ${elem.tagName}`);
+  });
 }
