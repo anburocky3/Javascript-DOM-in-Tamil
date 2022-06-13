310f8c10b8679519ebbd191be111ae1f2f0ab5dc
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-06-13 22:17:28 +0530

    37-recorded

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -1,8 +1,26 @@
-const formEl = document.querySelector("form#learning");
-const inputTextEl = formEl.querySelector("input");
-const inputCheckboxEl = formEl.querySelector("input[type='checkbox']");
+const formEl = document.querySelector("form");
+const inputEl = document.querySelector("form input");
+const checkboxEl = document.querySelector("form input[type='checkbox']");
 
 formEl.addEventListener("submit", (event) => {
   event.preventDefault(); // Stops default behaviour
-  console.log(inputTextEl.value, inputCheckboxEl.checked);
+
+  // Task: Validate all input fields to respective rules ()
+  if (
+    inputEl.value != "" ||
+    inputEl.value.length <= 3 ||
+    inputEl.value.length > 20
+  ) {
+    alert("Validation Error: Name Invalid");
+    return;
+  }
+
+  if (!checkboxEl.checked) {
+    alert("Validation Error: Check the terms.");
+    return;
+  }
+
+  console.log("Form Submitted", inputEl.value, checkboxEl.checked);
+
+  console.log("DefaultPrevented:", event.defaultPrevented);
 });
