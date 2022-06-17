179b83ac6cc598466c9d1e5bd49bfc31deeed42a
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-06-17 15:42:25 +0530

    42-planned

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -1,23 +1,37 @@
-// const formEl = document.querySelector("form");
 const inputFullName = document.querySelector("#fullName input");
 
 // Get Forms using this
-const formEl = document.forms.feedback; // or this
-// const formEl = document.forms[0]; // or this
+const formEl = document.forms.feedback;
 
-// Get input Elements using document.elements (use by name)
-// const fullName = document.forms.feedback.elements.name; // or by index/bracket notation
-// const { name: fullName, email } = document.forms.feedback.elements; // or using Obj destructing
-const fullName = document.forms.feedback.name;
-console.log(fullName);
+const handleSubmit = (event) => {
+  event.preventDefault(); // Prevent default behaviour
 
-function handleInput(event) {
-  // access the value
-  console.log(event.target.value);
+  // construct a FormData object, which fires the formdata event
+  const formData = new FormData(formEl); // init
 
-  // access the form for validation purpose (backreference)
-  console.log(event);
-}
+  // formdata gets modified by the formdata event
+  console.log(formData.get("api-key"));
+};
 
-fullName.addEventListener("input", handleInput);
-// fullName.addEventListener("change", handleInput);
+const handleFormData = (event) => {
+  console.log("formdata fired");
+
+  // modifies the form data
+  const formData = event.formData;
+
+  // FormData has useful methods
+  formData.append("api-key", "authKey"); // append new key-value pairs
+  formData.entries(); // get entries
+  console.log([...formData.entries()]); // get entries
+  console.log([...formData.values()]); // get only values
+  console.log(formData.get("name")); // get individual form element's value
+  console.log(formData.getAll("category")); // returns an array of all the values by a key name.
+  console.log(formData.has("gender")); // returns true if the FormData object contains a key name.
+  console.log([...formData.keys()]); // returns an iterator of all the keys.
+  formData.set("hobbies", "Learning new things"); //sets a new value for an existing key name
+  formData.delete("hobbies"); // deletes the key
+  console.log([...formData.values()]); //returns an iterator of all the values in the FormData object.
+};
+
+formEl.addEventListener("submit", handleSubmit);
+formEl.addEventListener("formdata", handleFormData);
