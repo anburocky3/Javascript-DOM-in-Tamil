5218a4e9a865a1a7a79d9ca7e0d425ec1042a505
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-06-17 22:17:05 +0530

    44-planned

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -5,23 +5,36 @@ const handleSubmit = (event) => {
 
   const formData = new FormData(event.target);
 
-  // 1. Query String
-  // name=Xandra+Garrison&category=technical-support&email=hevabada%40mailinator.com&msg=Qui+voluptas+iure+se&terms=on
+  // 1. Construct Form into readable form
   const data = [...formData.entries()];
-  const asStringManual = data
-    .map((x) => `${encodeURIComponent(x[0])}=${encodeURIComponent(x[1])}`)
-    // or  .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
-    .join("&"); // old way
-
-  // Or Using this
   const asString = new URLSearchParams(formData).toString();
 
-  console.log(asString);
-
-  // 2. Using JSON
   const asJSON = JSON.stringify(Object.fromEntries(formData));
 
-  console.log(asJSON);
+  // 2. Send the formdata to backend
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://reqres.in/api/products/3", true); // API: https://reqres.in/
+  xhr.onload = function () {
+    console.log(xhr.responseText);
+  };
+  xhr.send();
+
+  // Or using fetch/axios
+  fetch("https://reqres.in/api/users", {
+    method: "POST",
+    headers: {
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+      "Content-Type": "application/json",
+    },
+    // body: asJSON,
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      document.querySelector("#responseTxt").innerText = JSON.stringify(res);
+      console.log(res);
+    });
 };
 
 formEl.addEventListener("submit", handleSubmit);
+
+// Task: Create a button and when button clicks, it should get IP address via fetch() using `https://api.ipify.org/?format=json`
