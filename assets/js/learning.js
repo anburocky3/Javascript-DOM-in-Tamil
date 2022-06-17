bd79d807a17d720883601544b0a40dfbe50ca4f0
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-06-17 21:38:52 +0530

    43-planned

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -1,37 +1,27 @@
-const inputFullName = document.querySelector("#fullName input");
-
-// Get Forms using this
 const formEl = document.forms.feedback;
 
 const handleSubmit = (event) => {
   event.preventDefault(); // Prevent default behaviour
 
-  // construct a FormData object, which fires the formdata event
-  const formData = new FormData(formEl); // init
+  const formData = new FormData(event.target);
 
-  // formdata gets modified by the formdata event
-  console.log(formData.get("api-key"));
-};
+  // 1. Query String
+  // name=Xandra+Garrison&category=technical-support&email=hevabada%40mailinator.com&msg=Qui+voluptas+iure+se&terms=on
+  const data = [...formData.entries()];
+  const asStringManual = data
+    .map((x) => `${encodeURIComponent(x[0])}=${encodeURIComponent(x[1])}`)
+    // or  .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+    .join("&"); // old way
 
-const handleFormData = (event) => {
-  console.log("formdata fired");
+  // Or Using this
+  const asString = new URLSearchParams(formData).toString();
 
-  // modifies the form data
-  const formData = event.formData;
+  console.log(asString);
 
-  // FormData has useful methods
-  formData.append("api-key", "authKey"); // append new key-value pairs
-  formData.entries(); // get entries
-  console.log([...formData.entries()]); // get entries
-  console.log([...formData.values()]); // get only values
-  console.log(formData.get("name")); // get individual form element's value
-  console.log(formData.getAll("category")); // returns an array of all the values by a key name.
-  console.log(formData.has("gender")); // returns true if the FormData object contains a key name.
-  console.log([...formData.keys()]); // returns an iterator of all the keys.
-  formData.set("hobbies", "Learning new things"); //sets a new value for an existing key name
-  formData.delete("hobbies"); // deletes the key
-  console.log([...formData.values()]); //returns an iterator of all the values in the FormData object.
+  // 2. Using JSON
+  const asJSON = JSON.stringify(Object.fromEntries(formData));
+
+  console.log(asJSON);
 };
 
 formEl.addEventListener("submit", handleSubmit);
-formEl.addEventListener("formdata", handleFormData);
