78138e984dd739de893649f31d0a8b7625c2e902
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-06-27 19:41:16 +0530

    42-recorded

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -1,17 +1,34 @@
-// const formEl = document.forms[0]; // by Index
-const formEl = document.forms.feedback; // by name
+const formEl = document.forms.feedback;
+const btnEl = document.querySelector("form button");
 
-// Get Elements from Form
-// const fullNameEl = formEl.elements[0]; // or by index/bracket notation
-const fullNameEl = formEl.elements.fullName; // or by index/bracket notation
-const typeEl = formEl.elements.type;
-const emailEl = formEl.elements.email;
-const descEl = formEl.elements.description;
-const termsEl = formEl.elements.terms;
+const handleSubmit = (event) => {
+  event.preventDefault(); // Prevent default behaviour
 
-// const fullNameEl = formEl.fullName; // you can also do this.
+  // construct a FormData object, which fires the formdata event
+  const formData = new FormData(formEl);
 
-// Object Destructing
-const { fullName, type, email, desc, terms } = formEl.elements;
+  // formdata gets modified by the formdata event
+  console.log("My API KEY IS: ", formData.get("api-key"));
+};
 
-console.log(fullName, type, email, desc, terms);
+const handleFormData = (e) => {
+  console.log("formdata fired!");
+
+  const formData = e.formData;
+
+  // useful methods
+  formData.append("api-key", "sadsadsadsadsadsa"); // append new key-value pairs
+  console.log([...formData.entries()]); // get entries
+  console.log([...formData.values()]); // get only values
+  console.log(formData.get("email")); // get individual form element's value
+  console.log(formData.getAll("type")); // returns an array of all the values by a key name
+  console.log(formData.has("gender")); // returns true if the FormData object contains a key name.
+  console.log([...formData.keys()]); // returns an iterator of all the keys.
+  formData.set("hobbies", "Learning new things"); //sets a new value for an existing key name
+  formData.delete("terms"); // deletes the key
+  console.log([...formData.values()]); //returns an iterator of all the values in the FormData object.
+};
+
+formEl.addEventListener("submit", handleSubmit);
+
+formEl.addEventListener("formdata", handleFormData);
