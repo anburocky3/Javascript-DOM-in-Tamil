6bf1ac1121d8480e4bcb3760f1cfacf27fac76de
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-06-30 16:28:33 +0530

    46-recorded

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -1,34 +1,32 @@
 const formEl = document.forms.feedback;
-const nameEl = formEl.elements.fullName;
+const categoryEl = formEl.elements.type;
+
+const inputCategory = document.querySelector("#input-categories");
+
+const allCategories = [...categoryEl];
 
 // Get Properties & Methods
-console.log(nameEl);
+console.log(categoryEl);
+console.log(allCategories);
 
 // 1. Attributes
-nameEl.value = "Anbu";
-nameEl.disabled = true;
-nameEl.readOnly = true;
+allCategories.forEach((category) => {
+  console.log(category.value);
+  console.log(category.checked);
+});
 
 // 2. Events
-nameEl.addEventListener("focus", () => console.log("focused!"));
-nameEl.addEventListener("blur", () => console.log("blur"));
-nameEl.addEventListener("input", (e) =>
-  console.log(`You have entered: ${e.target.value}`)
-);
-nameEl.addEventListener("change", (e) =>
-  console.log(`Changed value: ${e.target.value}`)
-);
-
-// Other events
-nameEl.addEventListener("cut", () => console.log("cut"));
-nameEl.addEventListener("copy", () => {
-  alert("You can't copy the values");
+inputCategory.addEventListener("change", (event) => {
+  const checked = allCategories.find((category) => {
+    return category.checked;
+  });
+  console.log("Selected: ", checked.value);
+  console.log("Simple: ", event.target.value);
 });
-nameEl.addEventListener("paste", () => console.log("paste"));
 
 // 3. Methods
-nameEl.focus();
-nameEl.blur();
+allCategories[1].select();
+allCategories[2].checked = true;
 
 const handleSubmit = (event) => {
   event.preventDefault();
@@ -38,10 +36,15 @@ const handleSubmit = (event) => {
 
 formEl.addEventListener("submit", handleSubmit);
 
-// Task: Task: Create a One-way binding, when the name field changes, it should great like `Hello, {enteredInput} in real time.`
+// Task:  If contribution radio button clicked, then show an alert message of "Thank you for contributing." after 1 Sec.
 
-nameEl.addEventListener("change", (event) => {
-  document.querySelector(
-    "#nameGreet"
-  ).innerText = `Hello ${event.target.value}, `;
+inputCategory.addEventListener("change", (event) => {
+  const checked = allCategories.find((category) => category.checked);
+  // const checked = event.target;
+
+  if (checked.value === "contribution") {
+    setTimeout(() => {
+      alert("Thank you for contributing!");
+    }, 1000);
+  }
 });
