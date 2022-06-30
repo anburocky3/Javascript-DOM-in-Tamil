786de2fa89c38f76120cea7f0b03f51e7dabfcdf
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-06-30 15:27:54 +0530

    45-recorded

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -1,36 +1,47 @@
 const formEl = document.forms.feedback;
-const btnEl = document.querySelector("form button");
+const nameEl = formEl.elements.fullName;
+
+// Get Properties & Methods
+console.log(nameEl);
+
+// 1. Attributes
+nameEl.value = "Anbu";
+nameEl.disabled = true;
+nameEl.readOnly = true;
+
+// 2. Events
+nameEl.addEventListener("focus", () => console.log("focused!"));
+nameEl.addEventListener("blur", () => console.log("blur"));
+nameEl.addEventListener("input", (e) =>
+  console.log(`You have entered: ${e.target.value}`)
+);
+nameEl.addEventListener("change", (e) =>
+  console.log(`Changed value: ${e.target.value}`)
+);
+
+// Other events
+nameEl.addEventListener("cut", () => console.log("cut"));
+nameEl.addEventListener("copy", () => {
+  alert("You can't copy the values");
+});
+nameEl.addEventListener("paste", () => console.log("paste"));
+
+// 3. Methods
+nameEl.focus();
+nameEl.blur();
 
 const handleSubmit = (event) => {
   event.preventDefault();
 
   const formData = new FormData(formEl);
-
-  const jsonData = JSON.stringify(Object.fromEntries(formData));
-
-  // Send to Backend
-  // 1. XMLHttpRequest
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://reqres.in/api/users/2", true);
-  xhr.onload = function () {
-    const obj = JSON.parse(xhr.responseText);
-    document.getElementById("response").innerText = obj.data.first_name;
-  };
-  xhr.send();
-
-  // 2. fetch(), axios()
-  fetch("https://reqres.in/api/users?page=2", {
-    method: "GET",
-    // headers: {
-    //   // 'Content-Type': 'application/x-www-form-urlencoded' // MIME
-    //   "Content-Type": "application/json", // MIME
-    // },
-    // body: jsonData,
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      document.getElementById("response").innerText = JSON.stringify(data.data);
-    });
 };
 
 formEl.addEventListener("submit", handleSubmit);
+
+// Task: Task: Create a One-way binding, when the name field changes, it should great like `Hello, {enteredInput} in real time.`
+
+nameEl.addEventListener("change", (event) => {
+  document.querySelector(
+    "#nameGreet"
+  ).innerText = `Hello ${event.target.value}, `;
+});
