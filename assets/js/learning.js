3d1525f6a556938f3b57a8ebb08381b74215b4c1
jegannathanpc <107254052+jegannathanpc@users.noreply.github.com>
2022-11-21 11:20:07 +0530

    Update learning.js

--- assets/js/learning.js
+++ assets/js/learning.js
@@ -9,22 +9,22 @@ document.addEventListener("keyup", (event) => {
 });
 
 // Task: Build a simple typing-test screen.
-// document.addEventListener("keyup", (event) => {
-//   switch (event.key) {
-//     case "ArrowUp":
-//       alert("You have pressed ArrowUp Key");
-//       break;
-//     case "ArrowDown":
-//       alert("You have pressed ArrowDown Key");
-//       break;
-//     case "ArrowLeft":
-//       alert("You have pressed ArrowLeft Key");
-//       break;
+document.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+      alert("You have pressed ArrowUp Key");
+      break;
+    case "ArrowDown":
+      alert("You have pressed ArrowDown Key");
+      break;
+    case "ArrowLeft"
+      alert("You have pressed ArrowLeft Key");
+      break;
 //     case "ArrowRight":
 //       alert("You have pressed ArrowRight Key");
 //       break;
-//     default:
-//       alert("Unknown key detected");
-//       break;
-//   }
-// });
+    default:
+      alert("Unknown key detected");
+      break;
+   }
+ });
